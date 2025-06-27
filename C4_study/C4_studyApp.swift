6d46dbfcc62354fff19765560b366b070fb26ff9
Cherry <r0mancesi1z0@gmail.com>
2025-06-27 09:27:59 +0900

    Initial Commit

---
+++ C4_study/C4_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  C4_studyApp.swift
+//  C4_study
+//
+//  Created by 서연 on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct C4_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C4_study/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  C4_study
+//
+//  Created by 서연 on 6/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
